f0b79af30e2c90991299ea9dc966514cb4525a59
jb830 <jbrumfield623@gmail.com>
2024-03-23 05:53:11 -0700

    code attribution

--- week-1/hello-world/src/app/app.component.ts
+++ week-1/hello-world/src/app/app.component.ts
@@ -1,3 +1,13 @@
+/*
+============================================
+; Title:  app.components.ts 
+; Author: Professor Krasso
+; Date: 23. March, 2024
+; Modified by: Joanna Brumfield
+; Description: Component
+;===========================================
+*/
+
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
